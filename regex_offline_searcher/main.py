9eebd14e712f7ee567bc681346174a09ee0bf0e8
Hod Vaknin <hodvak@gmail.com>
2023-07-30 02:25:22 +0300

    remove re.LOCAL flag

--- regex_offline_searcher/main.py
+++ regex_offline_searcher/main.py
@@ -3,13 +3,11 @@ import re
 
 eel.init('web', allowed_extensions=['.js', '.html'])
 
-print(int(re.ASCII), int(re.DOTALL), int(re.IGNORECASE), int(re.MULTILINE), int(re.VERBOSE), int(re.LOCALE))
 
 @eel.expose
 def check_findall(regex, text, flags=0):
     regex = regex.replace('\xa0', ' ')
     text = text.replace('\xa0', ' ')
-    print(repr(regex), repr(text), repr(flags))
     if not regex:
         eel.send_data([])
     good_data = []
